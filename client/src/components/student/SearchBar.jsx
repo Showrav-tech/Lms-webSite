5baf8f471627bf254337921d5f3abd57b22007a2
Showrav <showravc85@gmail.com>
2026-06-28 20:10:16 +0600

    Completed SearchBar

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { assets } from "../../assets/assets";
-const SearchBar = () => {
+
+const SearchBar = ({ data }) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data || "");
+
+  const onSearchhandler = (e) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
+
   return (
-  
-     <form className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded overflow-hidden">
+    <form
+      onSubmit={onSearchhandler}
+      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded overflow-hidden"
+    >
+      <img
+        src={assets.search_icon}
+        alt="search"
+        className="w-10 px-3"
+      />
 
-  <img
-    src={assets.search_icon}
-    alt="search"
-    className="w-10 px-3"
-  />
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search for courses"
+        className="w-full h-full outline-none text-gray-500/80"
+      />
 
-  <input
-    type="text"
-    placeholder="Search for courses"
-    className="w-full h-full outline-none text-gray-500/80"
-  />
-
-  <button
-    type="submit"
-    className="h-full bg-blue-600 text-white px-6 md:px-10 
-   rounded hover:bg-blue-700 transition"
-  >
-    Search
-  </button>
-
-</form>
-   
-  )
-}
+      <button
+        type="submit"
+        className="h-full bg-blue-600 text-white px-6 md:px-10 hover:bg-blue-700 transition"
+      >
+        Search
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
