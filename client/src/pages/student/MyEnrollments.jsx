b2a5d28e4e7f76ef24579def4ea3bf31c2629aa6
Showrav <showravc85@gmail.com>
2026-07-11 13:15:56 +0600

    feat(ui): create MyEnrollments page layout

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const MyEnrollments = () => {
   return (
-    <div>
-      
+    <>
+    <div className='md:px-36 px-8 pt-10'>
+      <h1 className='text-2xl font-semibold'>My Enrollmnets</h1>
+      <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
+        <thead className='text-gray-900 border-gray-500/20 text-sm text-left max-sm:hidden'>
+          <tr>
+            <th className='px-4 py-3 font-semibold truncate'>Course</th>
+         <th className='px-4 py-3 font-semibold truncate'>Duration</th>
+      <th className='px-4 py-3 font-semibold truncate'>Completed</th>
+        <th className='px-4 py-3 font-semibold truncate'>Status</th>
+          </tr>
+        </thead>
+      </table>
     </div>
+    </>
   )
 }
 
